96737e12f20cec41d383001bf175423f8be7f87b
Leeann Drees <leeann.drees@detroitlabs.com>
2018-10-25 13:24:33 -0400

    new model NameData for things where we only care about the name (species, homeworld)

---
+++ StarWars/Model/NameData.swift
@@ -0,0 +1,15 @@
+//
+//  NameData.swift
+//  StarWars
+//
+//  Created by DetroitLabs on 10/25/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+class NameData: Codable {
+    
+    var name = ""
+    
+}
